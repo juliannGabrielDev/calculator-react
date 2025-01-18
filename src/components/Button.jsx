320ab50d5a64f8bbdeb90ba6be9e6e968b6bd82e
JuliannGabrielDev <julian.alejandro.gabriel@gmail.com>
2025-01-17 22:41:00 -0600

    17 - 01 - 2024

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ children, value, onClick, className }) => {
 
   return (
     <button
-      onClick={onClick}
+      onClick={(e) => onClick && onClick({ ...e, target: { ...e.target, value } })}
       className={`${className} flex justify-center items-center rounded-[75px] h-20 bg-lime-100 active:bg-green-400 active:scale-75 transition ease-in duration-300`}
       value={value}
     >
@@ -16,7 +16,6 @@ const Button = ({ children, value, onClick, className }) => {
 Button.propTypes = {
   children: PropTypes.node.isRequired,
   value: PropTypes.string,
-  type: PropTypes.string,
   onClick: PropTypes.func,
   className: PropTypes.string,
 };
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Button from "../components/Button";
 import {
   Clear,
@@ -22,17 +23,33 @@ import {
 } from "../assets/icons";
 
 const Calculator = () => {
+  const [inputValue, setInputValue] = useState("0");
+
   const style="h-1/2";
+  const clear = () => {
+    setInputValue("0");
+  }
+
+  const handleClick = (e) => {
+    if (inputValue == "0") {
+      setInputValue(`${e.target.value}`);
+    } else {
+      setInputValue((prev) => `${(prev)}${e.target.value}`);
+    }
+  }
 
   return (
     <main className="main grid md:grid-cols-2">
-      <section className="">hi</section>
+      <section className="px-8 py-5">
+        <label htmlFor="output" className="font-bold">Result:</label>
+        <input type="text" value={inputValue} id="output" readOnly className="bg-gray-100 text-6xl w-full h-32 p-5 rounded-3xl" />
+      </section>
       <section className="grid grid-cols-4 gap-3 px-8 py-5">
-        <Button className="bg-green-200"><img src={Clear} alt="Clear" className={style} /></Button>
+        <Button onClick={clear} className="bg-green-200"><img src={Clear} alt="Clear" className={style} /></Button>
         <Button className="bg-lime-200"><img src={Percentage} alt="Percentage" className={style} /></Button>
         <Button className="bg-lime-200"><img src={Divide} alt="Clear" className={style} /></Button>
         <Button className="bg-lime-200"><img src={XIcon} alt="Clear" className={style} /></Button>
-        <Button><img src={Number1} alt="Clear" className={style} /></Button>
+        <Button onClick={handleClick} value="1"><img src={Number1} alt="1" className={style} /></Button>
         <Button><img src={Number2} alt="Clear" className={style} /></Button>
         <Button><img src={Number3} alt="Clear" className={style} /></Button>
         <Button className="bg-lime-200"><img src={Minus} alt="Clear" className={style} /></Button>
